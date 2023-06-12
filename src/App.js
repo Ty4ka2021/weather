519fcd7eb578c5e773772834fe8aebc225f8538d
Dmitriy <dm.hmara@gmail.com>
2023-06-12 14:34:23 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import './App.css';
-import Info from './component/Info/Info';
-import House from './assets/House.png'
-
-import Modal from './component/Modal/Modal';
+import './App.css'
 
 function App() {
 
-  let currentCity = 'Kyiv';
+  let currentCity = 'Kyiv'
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=bf35cac91880cb98375230fb443a116f')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
+  // fetch('https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=bf35cac91880cb98375230fb443a116f')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
 
 
   return (
     <div className="App">
-      <Info currentCity={currentCity} />
-      <img className='house' src={House} alt="House" />
-      <Modal />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
